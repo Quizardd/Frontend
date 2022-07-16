636808d555846083313fb7bd4aba3e9ce1a7cfba
YoitsVansh <vansh.199301021@muj.manipal.edu>
2022-07-16 13:13:10 +0530

    Basic boilerplate code with home page ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Exam from "./Assets/Exam.svg"
+import Wizard from "./Assets/Wizard.gif"
+import Arrow from "./Assets/Arrow.png"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className='h-full flex items-center justify-center'>
+			<div className='h-3/5 w-full flex'>
+				<div className='w-1/3 h-full'>
+					<img src={Exam} />
+				</div>
+				<div className='w-2/5 h-full flex flex-col justify-center items-center pt-4'>
+					<h1 className='text-center text-primaryBlue text-8xl tracking-wider font-bold'>
+						QUIZARD
+					</h1>
+					<h1 className=' text-center font-semibold text-[#494554] text-lg'>
+						Your witty wizard to create online quizes
+					</h1>
+		
+					<a
+						href='/login'
+						className='w-fit py-3 px-16 mt-12 rounded-2xl text-white bg-[#D53CBB]'>
+						Get Started
+					</a>
+				</div>
+				<div className='w-1/3 h-full'>
+					<img src={Wizard} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primaryBlue: "#6C63FE",
+			},
+		},
+	},
+	plugins: [],
+};
